2ac85763dfc927ab854ad200ad70d63a50d5eed9
maksimkaSAS <knms1b22.sas@kpnu.edu.ua>
2025-08-20 10:53:23 +0300

    image viewer

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-// // Отримуємо поточний рік
-// const currentYear = new Date().getFullYear();
-
-// // Встановлюємо значення року в елемент із id "year"
-// document.getElementById('year').textContent = currentYear;
-
-
 // Динамічний рік у футері
 document.getElementById("year").textContent = new Date().getFullYear();
+
+// Lightbox
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.querySelector(".lightbox-img");
+const closeBtn = document.querySelector(".close-btn");
+
+document.querySelectorAll(".project-img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.classList.add("active");
+    lightboxImg.src = img.src; // вставляємо шлях до картинки
+    lightboxImg.alt = img.alt;
+  });
+});
+
+// Закриття по кліку на хрестик
+closeBtn.addEventListener("click", () => {
+  lightbox.classList.remove("active");
+});
+
+// Закриття по кліку поза картинкою
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.classList.remove("active");
+  }
+});
